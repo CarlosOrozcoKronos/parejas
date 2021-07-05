d549325661acd1d037c4c3144998f52b5b40dc77
KRONO\Carlos <adan_alda@hotmail.com>
2021-07-05 10:46:17 +0200

    Se comprueban si las cartas son parejas

--- js/js.js
+++ js/js.js
@@ -1,10 +1,34 @@
 const CARTASTOTALES = 4;
+var carta1, carta2;
 
 function girar() {
   let element = document.getElementById(event.target.id);
+  if (carta1 == undefined){
+    carta1 = element
+  }
+  else if (carta2 == undefined){
+    carta2 = element
+  }
   //resetMe(element)
   element.classList.remove("oculto");
   element.classList.add("girada");
+
+  if (!(carta1 == undefined) && !(carta2 == undefined)){
+    //si son pareja...
+    if ((carta1.classList.contains("rojo") && carta2.classList.contains("rojo"))
+    ||
+    (carta1.classList.contains("verde") && carta2.classList.contains("verde"))){
+      console.log("una pareja");
+    }
+    else{
+      resetMe(carta1);
+      resetMe(carta2);
+    }
+
+    carta1 = undefined;
+    carta2 = undefined;
+    
+  }
 }
 
 function resetMe(carta) {
